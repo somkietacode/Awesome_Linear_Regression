d81ccf692cd4f14f1086e8872564733033adcf09
somkietacode <108131461+somkietacode@users.noreply.github.com>
2022-10-20 22:12:22 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Awesome_Linear_Regression',
+    version='1.0.0',
+    install_requires=[
+        'numpy',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+)
